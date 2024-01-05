6138d0c6031a38ac66a865c8db5f42654f5fb5e4
Aleksa013 <velma86@bk.ru>
2024-01-05 15:49:11 +0200

    feat: add resolution getStringsLength and removeFalsyValues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -107,10 +107,17 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter(
+    (item) =>
+      item !== false &&
+      item !== null &&
+      item !== 0 &&
+      item !== undefined &&
+      !Number.isNaN(item) &&
+      item.length !== 0
+  );
 }
-
 /**
  * Returns an array containing the lengths of each string in a specified array of strings.
  *
@@ -121,8 +128,8 @@ function removeFalsyValues(/* arr */) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((x) => x.length);
 }
 
 /**
